fee8f10b95d911701e1e9444fbd87588a06474eb
Jesus Salcedo <jesalceg@gmail.com>
2024-11-09 21:43:20 -0500

    feature: Add register feature logic

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,4 +14,50 @@ import { Component } from '@angular/core';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm: FormGroup;
+
+  passwordMatchValidator(formGroup: AbstractControl): ValidationErrors | null {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  constructor(private fb: FormBuilder, public authService: AuthService) {
+    this.signupForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.passwordMatchValidator } //Validate our password
+    );
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      try {
+        const { email, password } = this.signupForm.value;
+        const response = this.authService.signUp(email, password);
+      } catch (error) {
+        console.log('Error is ', error);
+      }
+    } else {
+      console.log('Formulario inválido');
+      this.signupForm.markAllAsTouched();
+    }
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
+  }
+}
